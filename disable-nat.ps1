33edc69ebd9eb933cbebc4136fdc5a854bbe298c
Ciberth <thomasclauwaert@gmail.com>
2024-09-09 17:30:56 +0200

    Added app.jar, web and db config, created disable-nat powershell script

---
+++ disable-nat.ps1
@@ -0,0 +1,10 @@
+﻿
+# Don't disconnect the NAT interface from the ISP router, or you can't reach the internet!
+# Verify the vboxmanage path below, when needed
+
+$vms = @("dns", "web", "companyrouter","dns","database","red","homerouter","employee")
+
+ForEach($vm in $vms) {
+    echo "Disabling NAT for $vm"
+    & "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" controlvm $vm setlinkstate1 off
+}
